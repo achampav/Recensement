6ff37e679331e4b19701601a096af21816df7995
achampav <alexandra@champavert-consulting.fr>
2018-02-06 11:02:52 +0100

    Revert "Ajustement charset"
    This reverts commit fb9faa271b092c76f81f05a9c383a68060e490bf.

--- select_departements.php
+++ select_departements.php
@@ -1,19 +1,18 @@
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=windows-1252" />
 </head>
 <body>
 <table>
 <?php
 $db_username = "recensement";
 $db_password = "recensement";
-$db = "oci:dbname=recensement;charset=WE8MSWIN1252";
+$db = "oci:dbname=recensement";
 try{
 $conn = new PDO($db,$db_username,$db_password);
 $stmt = $conn->prepare("select dep,dep_desc from dep");
 $stmt->execute();
 while($row = $stmt->fetch(PDO::FETCH_ASSOC,PDO::FETCH_ORI_NEXT)){
-    printf("<tr><td>%s</td><td>%s</td></tr><br/>\n",$row["DEP"],$row["DEP_DESC"]);
+    printf("<tr><td>%s</td><td>%s</td></tr><br/>",$row["dep"],$row["dep_desc"]);
 }
 }catch(Exception $e){
     echo $e->getMessage(),"\n";
